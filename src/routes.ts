4118a1dcd4e76138aff9c6302e2d3154793135bb
Julio Dutra <jjcdutra@gmail.com>
2020-04-22 21:43:57 -0300

    Tipando objetos e vetores

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from 'express';
 
+import createUser from './services/createUser';
+
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'julio@rocketseat.com.br',
+    password: '123456',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native',
+      { title: 'Javascript', experience: 100 },
+    ],
+  });
+
   return response.json({ message: 'Hello World' });
 }
